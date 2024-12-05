d21043bcd155fe180510c93108e00ed791caa9ed
momal-26 <85959276+momal-26@users.noreply.github.com>
2024-12-05 22:54:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function myButton() {
+    var x = document.getElementById("myDIV");
+    if (x) { // Check that x is not null  
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        }
+        else {
+            x.style.display = "none";
+        }
+    }
+    else {
+        console.error("Element with ID 'myDIV' not found.");
+    }
+}
